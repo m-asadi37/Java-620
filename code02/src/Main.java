072c6df493115dceebae14e3729a3682bdb667a6
m.asadi <masd9617@gmail.com>
2025-11-12 00:13:18 +0330

    session 2

---
+++ code02/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // decimal
+
+        // byte - 1 = 8 bit -> -128 - 127
+        // short - 2
+        // int - 4
+        // long - 8
+
+        byte b1;
+        b1 = 12;
+        System.out.println(b1);
+        b1 = -14;
+        System.out.println(b1);
+
+        short s1 = 10;
+
+        // *int* - long (L,l)
+        int i1 = 10, i2 = 4;
+
+        long l0 = 120L;
+
+        long l_k$12;
+        long ll1 = 10, LL1 = 10;
+
+        // floating number
+        // float - 4 (F,f)
+        // *double* - 8
+        float f1 = 10.2f;
+        double d1 = 10.2;
+    }
+}
