18ea1e6ef80fe9e6f360aee4c14c74acba2e9010
m.asadi <masd9617@gmail.com>
2025-12-07 18:10:07 +0330

    session 08

---
+++ code08/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+
+        Person person1 = new Person();
+        person1.name = "faranak";
+        person1.family = "lorpoor";
+        person1.age = 16;
+
+        Person person2 = new Person();
+        person2.name = "aida";
+        person2.family = "rostami";
+        person2.age = 17;
+    }
+
+}
---
+++ code08/src/Person.java
@@ -0,0 +1,16 @@
+public class Person {
+
+    //property
+    //[data type] [name];
+    String name;
+    String family;
+    int age;
+
+    //method
+    //[return type] [method name] ([arg1], [arg2], ..){
+    // code...
+    // }
+    void sayHello(){
+        System.out.println("Hello!!!");
+    }
+}
