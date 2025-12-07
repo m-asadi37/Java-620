b10cc229bd380e7ef01f74d25bc6d55475e1228e
m.asadi <masd9617@gmail.com>
2025-12-07 18:17:27 +0330

    session 08 append

--- code08/src/Main.java
+++ code08/src/Main.java
@@ -13,6 +13,11 @@ public class Main {
         person2.name = "aida";
         person2.family = "rostami";
         person2.age = 17;
+
+        String name = person1.name;
+        System.out.println("person1 name is " + name);
+        System.out.println("person2 name is " + person2.name);
+
     }
 
 }
