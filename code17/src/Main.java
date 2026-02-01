345d53c79944e7c2012a98a402152ddfa3b80c99
m.asadi <masd9617@gmail.com>
2026-02-01 19:04:52 +0330

    session 17 append

--- code17/src/Main.java
+++ code17/src/Main.java
@@ -1,6 +1,10 @@
+import com.github.mfathi91.time.PersianDate;
+import com.github.mfathi91.time.PersianMonth;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
@@ -38,7 +42,7 @@ public class Main {
         System.out.println(str1);
     }
 
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
         LocalDate date1 = LocalDate.of(2025, 10, 16);
         System.out.println(date1);
 
@@ -57,5 +61,45 @@ public class Main {
 
         OffsetDateTime ofst = now.atOffset(ZoneOffset.of("-02:00"));
         System.out.println(ofst);
+    }
+
+    public static void main3(String[] args) {
+        Instant ins = Instant.now();
+        System.out.println(ins);
+
+        OffsetDateTime oft = ins.atOffset(ZoneOffset.of("+03:30"));
+        System.out.println(oft);
+
+        ZonedDateTime zdt = ins.atZone(ZoneId.of("Asia/Tehran"));
+        System.out.println(zdt);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime dateTime = LocalDateTime.now();
+        String str = dateTime.format(formatter);
+        System.out.println(str);
+
+        LocalDateTime dateTime1 = LocalDateTime.parse("2026-02-01 18:50:36", formatter);
+        System.out.println(dateTime1);
+    }
+
+    public static void main(String[] args) {
+// Instantiate
+        PersianDate today = PersianDate.now();
+        PersianDate persianDate1 = PersianDate.of(1396, 7, 15);
+        PersianDate persianDate2 = PersianDate.of(1396, PersianMonth.MEHR, 15);
+
+// Convert
+        PersianDate persianDate5 = PersianDate.of(1397, 5, 11);
+        LocalDate gregDate = today.toGregorian();    // => '2018-08-02'
+        PersianDate persianDate6 = PersianDate.fromGregorian(gregDate);  //  => '1397/05/11'
+
+// Parse
+        PersianDate persianDate3 = PersianDate.parse("1400-06-15");    // From the standard format
+        PersianDate persianDate4 = PersianDate.parse("1400/06/15", DateTimeFormatter.ofPattern("yyyy/MM/dd"));    // From a desired format
+
+// Format
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        String s1 = dtf.format(PersianDate.now());    // => e.g. '1396/05/10'
+        System.out.println(s1);
     }
 }
