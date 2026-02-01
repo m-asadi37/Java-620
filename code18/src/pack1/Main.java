8ad1fc6e650cc57fadeaf369ded1881484168c03
m.asadi <masd9617@gmail.com>
2026-02-01 20:22:32 +0330

    session 18

---
+++ code18/src/pack1/Main.java
@@ -0,0 +1,13 @@
+package pack1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.publicProp = "p1";
+        person.ProtectedProp = "p2";
+        person.defaultProp = "p3";
+//        person.privateProp = "p4";
+
+    }
+}
---
+++ code18/src/pack1/Person.java
@@ -0,0 +1,9 @@
+package pack1;
+
+public class Person {
+
+    public String publicProp;
+    private String privateProp;
+    protected String ProtectedProp;
+    String defaultProp;
+}
